f301ebce513e70713cf96f816fc0f26387796a2c
ShravanMCAnu <bskmc850@gmail.com>
2022-11-05 16:19:21 +0530

    Add project files.

---
+++ Test1_ShravanKumar_Project4/Program.cs
@@ -0,0 +1,19 @@
+﻿using Test1_ShravanKumar_Project4;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Principle Amount :");
+        double principleAmount = Convert.ToDouble(Console.ReadLine());
+        SimpleIntrest objSimpleIntrest=new SimpleIntrest();
+        Console.WriteLine("Enter Rate Of Intrest: ");
+        objSimpleIntrest.RateOfInterest = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter Time ");
+        objSimpleIntrest.Time = Convert.ToDouble(Console.ReadLine()); 
+        objSimpleIntrest.IntrestCalculation(principleAmount);
+        Console.WriteLine("the Intrest of Principle Amount is : " + objSimpleIntrest.Intrest);
+        Console.WriteLine("The Total Amount with Principle Amount and Intrest is : " + objSimpleIntrest.TotalAmount);
+
+    }
+}
---
+++ Test1_ShravanKumar_Project4/SimpleIntrest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_ShravanKumar_Project4
+{
+    public class SimpleIntrest
+    {
+        public double RateOfInterest { get; set; }
+        public double Time { get; set; }
+        public double TotalAmount { get; set; }
+        public double Intrest { get; set; }
+
+        public void IntrestCalculation(double _PrincipleAmount)
+        {
+            Intrest = (_PrincipleAmount * RateOfInterest * Time) / 100;
+
+            TotalAmount = _PrincipleAmount + Intrest;
+        }
+    }
+}
